10d46f6c0736c137da47b5c8b44abe27d78369f1
y8p9 <marcello.amaral1@hotmail.com>
2015-10-01 11:18:08 -0300

    estrutura inicial do projeto

---
+++ GitApi/GitApi.Web/Controllers/RepositoriosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitApi.Web.Controllers
+{
+    public class RepositoriosController : Controller
+    {
+        // GET: Repositorios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GitApi/GitApi.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitApi.Web.MvcApplication" Language="C#" %>
