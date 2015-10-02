fcb4b4dc2385f96518b6d1d72c0a47f47554397a
y8p9 <marcello.amaral1@hotmail.com>
2015-10-02 09:51:53 -0300

    readicionando arquivo de framework.

---
+++ GitApi/GitApi.Web/Scripts/views/framework.js
@@ -0,0 +1,11 @@
+﻿var Framework = {
+    Post: function (pUrl, parametros) {
+
+        return $.ajax({
+            type: "POST",
+            data: JSON.stringify(parametros),
+            url: pUrl,
+            contentType: 'application/json; charset=utf-8'
+        });
+    }    
+};
