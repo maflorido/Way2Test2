04f0d14620fbe328913f912915a26ff0c10406f9
y8p9 <marcello.amaral1@hotmail.com>
2015-10-06 12:44:48 -0300

    2

---
+++ GitApi/GitApi.Domain/Repository/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace GitApi.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IList<T> Listar();
+
+        T Obter(long id);
+
+        List<T> Filtrar(Expression<Func<T, bool>> filtro);
+
+        void Incluir(T objeto);
+
+        void Editar(T objeto);
+
+        void Excluir(T objeto);
+    }
+}
---
+++ GitApi/GitApi.Domain/Repository/IProdutoRepository.cs
@@ -0,0 +1,11 @@
+﻿using Grocery.Domain.Entities;
+using Grocery.Repository;
+using System.Collections.Generic;
+
+namespace Grocery.Domain.Repository
+{
+    public interface IProdutoRepository : IGenericRepository<Produto>
+    {
+        IList<Produto> Listar(string orderBy, bool reverse);
+    }
+}
