22e9bb1f328e8313dec7af9b1c5b961e77f369f6
y8p9 <marcello.amaral1@hotmail.com>
2015-11-09 16:06:53 -0200

    exemplo de testes usando mock

---
+++ GitApi/GitApi.Tests/GitApi.Repository/GenericRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using GitApi.Domain.Entities;
+using GitApi.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GitApi.Tests.GitApi.Repository
+{
+    [TestClass]
+    public class GenericRepositoryTest
+    {
+        [TestMethod]
+        public void IncluirSuccess_Test()
+        {
+            var mock = new Mock<GenericRepository<Favoritos>>();
+
+            mock.Setup(x => x.Incluir(new Favoritos() { Id = 1, IdRepositorioGit = 1, NomeRepositorio = "teste" }));
+
+
+
+        }
+    }
+}
